c60a5f559cb1ee5aaeb42cccdc69f41c233a4ee9
DotNetCurry <dotnetcurry@a2zknowledgevisuals.com>
2015-01-20 08:11:10 +0530

    code added

---
+++ MVC5_Sessions/Controllers/CategoryController.cs
@@ -0,0 +1,39 @@
+﻿using System.Linq;
+using System.Web.Mvc;
+
+using MVC5_Sessions.Models;
+
+namespace MVC5_Sessions.Controllers
+{
+    [SessionState(System.Web.SessionState.SessionStateBehavior.Disabled)]
+    public class CategoryController : Controller
+    {
+        ApplicationEntities ctx;
+
+        public CategoryController()
+        {
+            ctx = new ApplicationEntities();
+        }
+
+        // GET: Category
+        public ActionResult Index()
+        {
+            var cats = ctx.Categories.ToList();
+            return View(cats);
+        }
+
+        public ActionResult SearchProducts()
+        {
+            return View();
+        }
+
+
+        [HttpPost]
+        public ActionResult SearchProducts(Category c)
+        {
+           TempData["Category"] = c;
+        //   Session["Category"] = c;
+            return  RedirectToAction("Index","Product");
+        }
+    }
+}
---
+++ MVC5_Sessions/Controllers/ProductController.cs
@@ -0,0 +1,52 @@
+﻿using System.Linq;
+using System.Web.Mvc;
+using MVC5_Sessions.Models;
+
+namespace MVC5_Sessions.Controllers
+{
+    public class ProductController : Controller
+    {
+
+        ApplicationEntities ctx;
+        public ProductController()
+        {
+            ctx = new ApplicationEntities(); 
+        }
+
+        // GET: Product
+        /// <summary>
+        /// Type Case the TempData to Category
+        /// </summary>
+        /// <returns></returns>
+        public ActionResult Index()
+        {
+            if (TempData["Category"] != null)
+        //    if (Session["Category"] != null)
+            {
+                var Cat = (Category)TempData["Category"];
+               // var Cat = (Category)Session["Category"];
+
+                var Products = from p in ctx.Products
+                               where p.CategoryId == Cat.CategoryId
+                               select p;
+
+                TempData.Keep("Category");
+
+                return View(Products.ToList());
+            }
+            else
+            {
+                return View(ctx.Products.ToList());
+            }
+        }
+        //  var Data = Session["Category"] as Category;
+        [HttpPost]
+        public ActionResult Index(int id=0) 
+        {
+            var Data = TempData["Category"] as Category;
+         
+
+            return RedirectToAction("Index");
+        }
+    }
+}
